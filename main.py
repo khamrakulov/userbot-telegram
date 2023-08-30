13b34d183dee28932554f6f49052f85d25f9df2c
Kamron Khamrakulov <khamrakulovkamron@gmail.com>
2023-08-30 06:13:04 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio
+from pyrogram import Client
+from dotenv.main import load_dotenv
+import os
+load_dotenv()
+
+api_id = os.environ["API_ID"]
+api_hash = os.environ["API_HASH"]
+
+
+async def main():
+    async with Client("my_account", api_id, api_hash) as app:
+        await app.send_message("me", "Greetings from **Pyrogram**!")
+
+
+asyncio.run(main())
