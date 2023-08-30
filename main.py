ff64d25c4362a6f13db4474d3881b0f56659aefb
Kamron Khamrakulov <khamrakulovkamron@gmail.com>
2023-08-30 06:17:46 +0500

    added session file

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ load_dotenv()
 api_id = os.environ["API_ID"]
 api_hash = os.environ["API_HASH"]
 
-
 async def main():
-    async with Client("my_account", api_id, api_hash) as app:
-        await app.send_message("me", "Greetings from **Pyrogram**!")
+    clientName = os.environ["client_name"]
 
+    async with Client(clientName, api_id, api_hash) as app:
+        await app.send_message("me", "Greetings from **Pyrogram**!")
 
 asyncio.run(main())
